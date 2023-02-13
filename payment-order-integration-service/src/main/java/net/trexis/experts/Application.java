0bc5e8f6130df5b38364b19968789d190702e346
Hendrik Tredoux <hendrik.tredoux@trexis.net>
2023-02-13 13:38:06 -0500

    Make ingestion async, so that the payment process complete.  And better error handling around delete responses.

--- payment-order-integration-service/src/main/java/net/trexis/experts/Application.java
+++ payment-order-integration-service/src/main/java/net/trexis/experts/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
